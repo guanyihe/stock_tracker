f48a82e9a7b24dfabcf8bec87326ae2482d9c8a2
Luke Guan <lukeguan@outlook.com>
2021-03-14 20:08:54 -0700

    add iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+  #self=> make it as a class method. it doesn't have to depend on instance var
+  #you can call the method directly 
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+    #return client.price(ticker_symbol)
+  end
 end
