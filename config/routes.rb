a53ee5d68b0c61750473462f879480137b19b60e
Luke Guan <lukeguan@outlook.com>
2021-03-17 23:41:17 -0700

    Add stock to the portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  #resources :user_stocks #it gives your all the route of user_stocks
+  resources :user_stocks, only: [:create]
+
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
